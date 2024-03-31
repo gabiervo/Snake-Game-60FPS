f08b052c7ef107c0d4aa736444715617b3dbd312
gabiervo <gabiervo@icloud.com>
2024-03-31 20:57:47 -0300

    added buffers and started adding animation

--- snakeMaps.cpp
+++ snakeMaps.cpp
@@ -10,42 +10,51 @@
 
 void checkKeys(int *k, std::vector<int> *inB) {
   if (IsKeyDown(KEY_RIGHT) && *k != KEY_LEFT) {
+    if ((*inB)[inB->size() - 1] != *k && inB->size() < 2) {
+      inB->push_back(*k);
+    }
     *k = KEY_RIGHT;
   }
   if (IsKeyDown(KEY_LEFT) and *k != KEY_RIGHT) {
+    if ((*inB)[inB->size() - 1] != *k && inB->size() < 2) {
+      inB->push_back(*k);
+    }
     *k = KEY_LEFT;
   }
   if (IsKeyDown(KEY_UP) && *k != KEY_DOWN) {
+    if ((*inB)[inB->size() - 1] != *k && inB->size() < 2) {
+      inB->push_back(*k);
+    }
     *k = KEY_UP;
   }
   if (IsKeyDown(KEY_DOWN) && *k != KEY_UP) {
+    if ((*inB)[inB->size() - 1] != *k && inB->size() < 2) {
+      inB->push_back(*k);
+    }
     *k = KEY_DOWN;
-  }
-  for (int i = 0; i < inB->size(); i++) {
-    inB->push_back(*k);
   }
 }
 
 void executeInput(std::vector<int> *buffer, int *xspd, int *yspd) {
   if (buffer->size() > 0) {
-    if ((*buffer)[0] == KEY_DOWN) {
+    if ((*buffer)[0] == KEY_DOWN && *yspd != -20) {
       *xspd = 0;
-      *yspd = 25;
+      *yspd = 20;
     }
-    if ((*buffer)[0] == KEY_UP) {
+    if ((*buffer)[0] == KEY_UP && *yspd != 20) {
       *xspd = 0;
-      *yspd = -25;
+      *yspd = -20;
     }
-    if ((*buffer)[0] == KEY_RIGHT) {
-      *xspd = 25;
+    if ((*buffer)[0] == KEY_RIGHT && *xspd != -20) {
+      *xspd = 20;
       *yspd = 0;
     }
-    if ((*buffer)[0] == KEY_LEFT) {
-      *xspd = -25;
+    if ((*buffer)[0] == KEY_LEFT && *xspd != 20) {
+      *xspd = -20;
       *yspd = 0;
     }
+    *buffer->erase(buffer->begin());
   }
-  *buffer->erase(buffer->begin());
 }
 
 bool shouldPlay = true;
@@ -72,13 +81,55 @@ int *snakeQueue(std::map<int, int *> *mp, int spdx, int spdy, int count,
   return new int[2]{oldX, oldY};
 }
 
+int drawAnimations(std::vector<int> inputBuf, int x, int y, int frame, int xspd,
+                   int yspd) {
+  if (inputBuf.size() > 0) {
+    int in = inputBuf[0];
+    switch (in) {
+    case KEY_DOWN:
+      DrawRectangle(x, y + (frame * 4), 20, 20, Color(WHITE));
+      break;
+    case KEY_UP:
+      DrawRectangle(x, y - (frame * 4), 20, 20, Color(WHITE));
+      break;
+    case KEY_RIGHT:
+      DrawRectangle(x + (frame * 4), y, 20, 20, Color(WHITE));
+      break;
+    case KEY_LEFT:
+      DrawRectangle(x - (frame * 4), y, 20, 20, Color(WHITE));
+      break;
+    }
+    return 0;
+  } else {
+    int valX = 0;
+    int valY = 0;
+    if (xspd != 0) {
+      valX = (((frame == 1) * 4) + ((frame == 2) * 8) + ((frame == 3) * 12) +
+              ((frame == 4) * 16));
+      if (xspd < 0) {
+        valX -= valX * 2;
+      }
+    }
+    if (yspd != 0) {
+      int valY = (((frame == 1) * 4) + ((frame == 2) * 8) +
+                  ((frame == 3) * 12) + ((frame == 4) * 16));
+      if (yspd < 0) {
+        valY -= valY * 2;
+      }
+    }
+
+    DrawRectangle(x + valX, y - valY, 20, 20, Color(BLUE));
+    return 1;
+  }
+}
+
 int main() {
   InitWindow(SCR_W, SCR_H, "testWindow");
 
   std::map<int, int *> m;
   m[0] = new int[2]{250, 250};
 
-  int size = 20;
+  int size = 3;
   int count = 1;
 
   int lastKey = KEY_DOWN;
@@ -91,6 +142,7 @@ int main() {
   arr[1] = 1;
   std::vector<int> inputBuffer;
   inputBuffer.push_back(KEY_DOWN);
+  int hasBuf;
 
   SetTargetFPS(60);
   if (IsWindowReady()) {
@@ -106,20 +158,24 @@ int main() {
       DrawText(std::to_string(arr[0]).c_str(), 200, 0, 20, Color(WHITE));
       DrawText(std::to_string(arr[1]).c_str(), 300, 0, 20, Color(WHITE));
 
-      // checkKeys(&lastKey, &inputBuffer);
-      executeInput(&inputBuffer, &xspd, &yspd);
+      checkKeys(&lastKey, &inputBuffer);
 
-      if (timeCount >= 6 && shouldPlay) {
+      if (timeCount >= 5 && shouldPlay) {
+        executeInput(&inputBuffer, &xspd, &yspd);
         arr = snakeQueue(&m, xspd, yspd, count, size, (size != m.size()));
         count++;
         timeCount = 0;
+      } else if (shouldPlay) {
+        hasBuf =
+            drawAnimations(inputBuffer, arr[0], arr[1], timeCount, xspd, yspd);
       }
+      DrawText(std::to_string(hasBuf).c_str(), 400, 0, 20, Color(WHITE));
       // loop to draw snakes
       std::map<int, int *>::iterator it;
       it = m.begin();
 
       for (it = it++; it != m.end(); ++it) {
-        DrawRectangle((it->second)[0], (it->second)[1], 25, 25, Color(WHITE));
+        DrawRectangle((it->second)[0], (it->second)[1], 20, 20, Color(WHITE));
       }
       EndDrawing();
     }
