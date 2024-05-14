cca629fcacf7a33cc4cec89403fc9d8c90d5f165
gabiervo <gabiervo@icloud.com>
2024-05-14 14:22:53 -0300

    finished the eyes, need to make the code less hacky

--- snakeMaps.cpp
+++ snakeMaps.cpp
@@ -1,5 +1,6 @@
 #include "/usr/local/Cellar/raylib/5.0/include/raylib.h"
 #include <cmath>
+#include <filesystem>
 #include <map>
 #include <string>
 #include <utility>
@@ -37,28 +38,76 @@ void checkKeys(int *k, std::vector<int> *inB) {
     *k = KEY_DOWN;
   }
 }
+void drawEyes(char characterDirection, int x, int y){
+  int posX1 = -1;
+  int posY1 = -1;
 
-void executeInput(std::vector<int> *buffer, int *xspd, int *yspd) {
+  int posX2 = -1;
+  int posY2 = -1;
+  if(characterDirection == 'r'){
+    posX1 = x + 13;
+    posX2 = posX1;
+
+    posY1 = y + 2;
+    posY2 = y + 13;
+  }
+  if(characterDirection == 'l'){
+    posX1 = x + 3;
+    posX2 = posX1;
+
+    posY1 = y + 2;
+    posY2 = y + 13;
+  }
+  if(characterDirection == 'u'){
+    posX1 = x + 2;
+    posX2 = x + 13;
+
+    posY1 = y + 3;
+    posY2 = posY1;
+  }
+  if(characterDirection == 'd'){
+    posX1 = x + 2;
+    posX2 = x + 13;
+
+    posY1 = y + 14;
+    posY2 = posY1;
+  }
+
+  if(posX1 != -1){
+    //DrawText(std::to_string(posX1).c_str(), 0, 400, 16, Color(WHITE));
+    DrawRectangle(posX1, posY1, 5, 5, Color(BLUE));
+    DrawRectangle(posX2, posY2, 5, 5, Color(BLUE));
+  }
+}
+
+char executeInput(std::vector<int> *buffer, int *xspd, int *yspd, char prevDirection) {
+  char dir = prevDirection; 
   if (buffer->size() > 0) {
     if ((*buffer)[0] == KEY_DOWN && *yspd != -20) {
       *xspd = 0;
       *yspd = 20;
+      dir = 'd';
     }
     if ((*buffer)[0] == KEY_UP && *yspd != 20) {
       *xspd = 0;
       *yspd = -20;
+      dir = 'u';
     }
     if ((*buffer)[0] == KEY_RIGHT && *xspd != -20) {
       *xspd = 20;
       *yspd = 0;
+      dir = 'r';
     }
     if ((*buffer)[0] == KEY_LEFT && *xspd != 20) {
       *xspd = -20;
       *yspd = 0;
+      dir = 'l';
     }
     *buffer->erase(buffer->begin());
   }
+  return dir;
 }
+
 
 bool shouldPlay = true;
 
@@ -114,9 +163,22 @@ int drawFrontAnimations(std::vector<int> inputBuf, int x, int y, int frame,
       break;
     case KEY_RIGHT:
       DrawRectangle(x + (frame * 4), y, 20, 20, color);
+
+      //eyes
+      DrawRectangle(x + (frame * 4) + 13, y + 2, 5, 5,
+		      Color(BLUE));
+      DrawRectangle(x + (frame * 4) + 13, y + 13, 5, 5,
+		      Color(BLUE));
       break;
     case KEY_LEFT:
       DrawRectangle(x - (frame * 4), y, 20, 20, color);
+	
+      //eyes
+      DrawRectangle(x - (frame * 4) + 3, y + 2, 5, 5,
+		      Color(BLUE));
+      DrawRectangle(x - (frame * 4) + 3, y + 13, 5, 5,
+		      Color(BLUE));
+
       break;
     }
     val = 2;
@@ -130,10 +192,14 @@ int drawFrontAnimations(std::vector<int> inputBuf, int x, int y, int frame,
         valX -= valX * 2;
       }
       DrawRectangle(x + valX, y, 20, 20, color);
-      DrawRectangle(x + valX + (valX < 0) * 3 + (valX > 0) * 13, y + 2, 5, 5,
+
+      //eyes
+      int finalValX = x + valX + (valX < 0) * 3 + (valX > 0) * 13;
+      DrawRectangle(finalValX, y + 2, 5, 5,
                     Color(BLUE));
       DrawRectangle(x + valX + (valX < 0) * 3 + (valX > 0) * 13, y + 13, 5, 5,
                     Color(BLUE));
+      DrawText(std::to_string(finalValX).c_str(), 0, 450, 16, Color(WHITE));
       val = 0;
       // if moving vertically
     } else {
@@ -165,8 +231,8 @@ void checkFoodCollision(std::pair<int, int> snakeHeadPos,
       *snakeSize += 1;
 
       //-1 and +1 to prevent rand() from returning nums bigger than screen size
-      (*foodPos).first = (rand() % ((SCR_W / TILE_SIZE) - 1) + 1) * TILE_SIZE;
-      (*foodPos).second = (rand() % ((SCR_H / TILE_SIZE) - 1) + 1) * TILE_SIZE;
+      (*foodPos).first = (rand() % ((SCR_W / TILE_SIZE) - 1)) * TILE_SIZE;
+      (*foodPos).second = (rand() % ((SCR_H / TILE_SIZE) - 1)) * TILE_SIZE;
     }
   } else {
     // DrawText("isOff", 0, 100, 20, Color(WHITE));
@@ -221,7 +287,7 @@ int main() {
   // necessary)
   m[0] = new int[2]{200, 240};
 
-  int size = 10;
+  int size = 2;
   int count = 1;
 
   int lastKey = KEY_DOWN;
@@ -237,19 +303,24 @@ int main() {
   std::vector<int> inputBuffer;
   inputBuffer.push_back(KEY_DOWN);
   int hasBuf;
+  char dir = 'd';
+  bool shouldDoFrontAnimations = false;
 
   int *secondToLastCoords = new int[2]{m[0][0], m[0][1]};
 
   SetTargetFPS(60);
   if (IsWindowReady()) {
-
+    if(size == 1){
+      shouldPlay = false;
+    }
     while (!WindowShouldClose()) {
+
       timeCount++;
       BeginDrawing();
       ClearBackground(BLACK);
 
       std::string fps =
-          std::to_string(std::round((1000 / GetFrameTime() / 1000)));
+        std::to_string(std::round((1000 / GetFrameTime() / 1000)));
       DrawText(fps.c_str(), 0, 0, 20, Color(WHITE));
       DrawText(std::to_string(arr[0]).c_str(), 200, 0, 20, Color(WHITE));
       DrawText(std::to_string(arr[1]).c_str(), 300, 0, 20, Color(WHITE));
@@ -257,21 +328,22 @@ int main() {
       checkKeys(&lastKey, &inputBuffer);
       if (shouldPlay) {
         drawBackAnimations(secondToLastCoords, m.begin()->second, timeCount,
-                           (size != m.size()));
+            (size != m.size()));
       }
       if (timeCount >= 5 && shouldPlay) {
-        executeInput(&inputBuffer, &xspd, &yspd);
+        dir = executeInput(&inputBuffer, &xspd, &yspd, dir);
         arr = snakeQueue(&m, xspd, yspd, count, size, (size != m.size()));
         checkFoodCollision(std::make_pair(m[count][0], m[count][1]), &foodPos,
-                           &size);
+            &size);
         count++;
         timeCount = 0;
+        shouldDoFrontAnimations = false;
       } else {
         if (shouldPlay) {
-          hasBuf = drawFrontAnimations(inputBuffer, arr[0], arr[1], timeCount,
-                                       xspd, yspd, m);
+          shouldDoFrontAnimations = true;
         }
       }
+      DrawText(std::to_string(dir).c_str(), 0, 200, 20, Color(WHITE));
       DrawText(std::to_string(hasBuf).c_str(), 400, 0, 20, Color(WHITE));
       // loop to draw snakes
       std::map<int, int *>::iterator it;
@@ -279,11 +351,21 @@ int main() {
       int countDrawnFrames = 0;
       for (it = ++it; it != m.end(); ++it) {
         countDrawnFrames++;
+
+        //fetch frames for second to last object to draw back
         if (countDrawnFrames == 1) {
           secondToLastCoords[0] = it->second[0];
           secondToLastCoords[1] = it->second[1];
         }
         DrawRectangle((it->second)[0], (it->second)[1], 20, 20, Color(WHITE));
+        if(timeCount == 0 && countDrawnFrames == m.size()-1){
+          //DrawText("drawing eyes", 0, 300, 16, Color(WHITE));
+          drawEyes(dir, (it->second)[0], (it->second)[1] );
+        }
+      }
+      if(shouldDoFrontAnimations){
+        hasBuf = drawFrontAnimations(inputBuffer, arr[0], arr[1], timeCount,
+            xspd, yspd, m);
       }
       DrawRectangle(foodPos.first, foodPos.second, 20, 20, Color(BLUE));
       EndDrawing();
